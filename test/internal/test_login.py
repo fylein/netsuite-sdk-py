a2cba01a4e626c31702f5a2bc9aa79d7fbbb53f0
Sivaramakrishnan Narayanan <siva@fyle.in>
2019-11-23 08:23:28 +0530

    Rename test directory to internal

--- test/internal/test_login.py
+++ test/internal/test_login.py
@@ -1,21 +1,25 @@
 import logging
 import os
+import time
 
 import pytest
 from netsuitesdk.zeep.client import NetSuiteClient
+from netsuitesdk.zeep.utils import PaginatedSearch
 
 logger = logging.getLogger(__name__)
 
-@pytest.fixture(scope='module')
-def ns():
+def test_login():
     """
+    Test if login method is supported. We will not use this often.
     """
     NS_EMAIL = os.getenv("NS_EMAIL")
     NS_PASSWORD = os.getenv("NS_PASSWORD")
     NS_ROLE = os.getenv("NS_ROLE")
     NS_ACCOUNT = os.getenv("NS_ACCOUNT")
     NS_APPID = os.getenv("NS_APPID")
-
     ns = NetSuiteClient(account=NS_ACCOUNT)
     ns.login(email=NS_EMAIL, password=NS_PASSWORD, role=NS_ROLE, application_id=NS_APPID)
-    return ns
+    type_name = 'Account'
+    paginated_search = PaginatedSearch(client=ns, type_name=type_name, pageSize=20)
+    assert len(paginated_search.records) > 0, f'There are no records of type {type_name}'
+    logger.debug('record = %s', str(paginated_search.records[0]))
