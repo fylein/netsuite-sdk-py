51fa6261c5238817dba8dee2559808350eef65be
Sravan Kumar <sravan.kumar@fyle.in>
2020-07-10 17:53:15 +0530

    adding few more asserts to tests

--- test/integration/test_expense_reports.py
+++ test/integration/test_expense_reports.py
@@ -9,10 +9,14 @@ def test_get(nc):
     data = next(nc.expense_reports.get_all_generator())
     logger.debug('data = %s', data)
     assert data, 'get all generator didnt work'
+    assert data['externalId'] == 'entity-5', f'No object found with externalId'
+    assert data['internalId'] == '-5', f'No object found with internalId'
 
     data = nc.expense_reports.get(externalId='EXPR_1')
     logger.debug('data = %s', data)
     assert data, f'No object with externalId'
+    assert data['externalId'] == 'EXPR_1', f'No object with externalId'
+    assert data['internalId'] == '10613', f'No object with internalId'
 
 def test_post(nc):
     filename = os.getenv('NS_ACCOUNT').lower() + '.json'
@@ -23,6 +27,10 @@ def test_post(nc):
     res = nc.expense_reports.post(expr1)
     logger.debug('res = %s', res)
     assert res['externalId'] == expr1['externalId'], 'External ID does not match'
+    assert res['type'] == 'expenseReport', 'Type does not match'
 
     expr2 = nc.expense_reports.get(externalId=res['externalId'])
     logger.debug('expr2 = %s', expr2)
+    assert expr2['amount'] == 100.0, 'Amount does not match'
+    assert expr2['externalId'] == 'EXPR_1', 'External ID does not match'
+    assert expr2['internalId'] == '10613', 'Internal ID does not match'
