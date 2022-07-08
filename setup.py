7919a9d7903027e13ee8014542707413144e0b46
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-07-08 17:52:51 +0530

    bump versiong

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='netsuitesdk',
-    version='2.12.1',
+    version='2.13.0',
     author='Siva Narayanan',
     author_email='siva@fyle.in',
     description='Python SDK for accessing the NetSuite SOAP webservice',
