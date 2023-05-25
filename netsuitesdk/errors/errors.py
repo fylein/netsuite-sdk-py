2c0f88dcfb311eff47901fbe5f125624c5ddf59a
Nilesh Pant <nileshpant6969@gmail.com>
2023-05-25 20:16:12 +0530

    refactor

--- netsuitesdk/errors/errors.py
+++ netsuitesdk/errors/errors.py
@@ -2,12 +2,12 @@
 error_reference = {
  "er": 
     {
-        "category_reference_error": r"An error occured in a upsert request: Invalid category reference key \d+ for entity \d+",
-        "account_reference_error": r"An error occured in a upsert request: Invalid account reference key \d+ for subsidiary \d+",
-        "project_reference_error": r"An error occured in a upsert request: Invalid customer reference key \d+ for entity \d+",
-        "location_reference_error": r"An error occured in a upsert request: Invalid location reference key \d+ for subsidiary \d+",
-        "department_reference_error": r"An error occured in a upsert request: Invalid department reference key \d+ for subsidiary \d+",
-        "currency_reference_error": r"An error occured in a upsert request: Invalid currency reference key \d+ for subsidiary \d+",
+        'category_reference_error': {'regex': r"An error occured in a upsert request: Invalid category reference key \d+ for entity \d+", 'keys': ['expense_category', 'employee']},
+        'account_reference_error': {'regex': r"An error occured in a upsert request: Invalid account reference key \d+ for subsidiary \d+", 'keys': ['account', 'subsidiary']},
+        'project_reference_error': {'regex': r"An error occured in a upsert request: Invalid customer reference key \d+ for entity \d+", 'keys': ['customer', 'employee']},
+        'location_reference_error': {'regex': r"An error occured in a upsert request: Invalid location reference key \d+ for subsidiary \d+", 'keys': ['location', 'subsidiary']},
+        'department_reference_error': {'regex':r"An error occured in a upsert request: Invalid department reference key \d+ for subsidiary \d+" , 'keys': ['department', 'subsidiary']},
+        'currency_reference_error': {'regex': r"An error occured in a upsert request: Invalid currency reference key \d+ for subsidiary \d+", 'keys': ['currency', 'subsidiary']}
     }
 }
 
--- netsuitesdk/errors/helpers.py
+++ netsuitesdk/errors/helpers.py
@@ -11,30 +11,12 @@ def convert_to_camelcase(word):
     return ''.join(word.title().split('_'))
 
 
+import re
+
 def export_error_matcher(string, export_type):
-
-    if re.match(error_reference[export_type]['category_reference_error'], string):
-        numbers = re.findall(r'\d+', string)
-        return {"expense_category": numbers[0], "employee": numbers[1]}
+    for _, error_data in error_reference[export_type].items():
+        if re.match(error_data['regex'], string):
+            numbers = re.findall(r'\d+', string)
+            return {key: int(number) for key, number in zip(error_data['keys'], numbers)}
     
-    if re.match(error_reference[export_type]['account_reference_error'], string):
-        numbers = re.findall(r'\d+', string)
-        return {"account": numbers[0], "subsidiary": numbers[1]}
-    
-    if re.match(error_reference[export_type]['project_reference_error'], string):
-        numbers = re.findall(r'\d+', string)
-        return {"customer": numbers[0], "employee": numbers[1]}
-    
-    if re.match(error_reference[export_type]['location_reference_error'], string):
-        numbers = re.findall(r'\d+', string)
-        return {"location": numbers[0], "subsidiary": numbers[1]}
-    
-    if re.match(error_reference[export_type]['department_reference_error'], string):
-        numbers = re.findall(r'\d+', string)
-        return {"department": numbers[0], "subsidiary": numbers[1]}
-    
-    if re.match(error_reference[export_type]['currency_reference_error'], string):
-        numbers = re.findall(r'\d+', string)
-        return {"currency": numbers[0], "subsidiary": numbers[1]}
-
     return {}
