c5648f1b23bd836e80eb82b9f18f4ed80d151006
Nilesh Pant <nileshpant6969@gmail.com>
2023-05-26 13:30:11 +0530

    ad

--- test/internal/test_parser.py
+++ test/internal/test_parser.py
@@ -9,6 +9,6 @@ def test_export_error_parser(mocker, ns):
         return_value={'John Doe', 'Travel'}
     )
 
-    parser = ErrorParser()
-    result = parser.export_error_parser({'expense_category': '1', 'employee': '12'}, ns, 'An error occured in a upsert request: Invalid category reference key 1 for entity 12')
+    parser = ErrorParser(ns)
+    result = parser.export_error_parser({'expense_category': '1', 'employee': '12'}, 'An error occured in a upsert request: Invalid category reference key 1 for entity 12')
     assert result == "An error occured in a upsert request: Invalid category reference key Travel for entity John Doe"
