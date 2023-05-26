a064027a7d0356b7f80aa63ad6aef34eabdc0517
Nilesh Pant <nileshpant6969@gmail.com>
2023-05-26 13:16:01 +0530

    fixed tests

--- test/internal/test_helper.py
+++ test/internal/test_helper.py
@@ -2,12 +2,12 @@ import pytest
 from netsuitesdk.errors.helpers import replace_numbers, convert_to_camelcase, export_error_matcher
 
 errors = [
-    ('An error occured in a upsert request: Invalid category reference key 1 for entity 12', {'expense_category': '1', 'employee': '12'}),
-    ('An error occured in a upsert request: Invalid account reference key 1 for subsidiary 12', {'account': '1', 'subsidiary': '12'}),
-    ('An error occured in a upsert request: Invalid customer reference key 1 for entity 12', {'customer': '1', 'employee': '12'}),
-    ('An error occured in a upsert request: Invalid location reference key 1 for subsidiary 12', {'location': '1', 'subsidiary': '12'}),
-    ('An error occured in a upsert request: Invalid department reference key 1 for subsidiary 12', {'department': '1', 'subsidiary': '12'}),
-    ('An error occured in a upsert request: Invalid currency reference key 1 for subsidiary 12', {'currency': '1', 'subsidiary': '12'})
+    ('An error occured in a upsert request: Invalid category reference key 1 for entity 12', {'expense_category': 1, 'employee': 12}),
+    ('An error occured in a upsert request: Invalid account reference key 1 for subsidiary 12', {'account': 1, 'subsidiary': 12}),
+    ('An error occured in a upsert request: Invalid customer reference key 1 for entity 12', {'customer': 1, 'employee': 12}),
+    ('An error occured in a upsert request: Invalid location reference key 1 for subsidiary 12', {'location': 1, 'subsidiary': 12}),
+    ('An error occured in a upsert request: Invalid department reference key 1 for subsidiary 12', {'department': 1, 'subsidiary': 12}),
+    ('An error occured in a upsert request: Invalid currency reference key 1 for subsidiary 12', {'currency': 1, 'subsidiary': 12})
 ]
 
 def test_replace_number():
@@ -30,5 +30,5 @@ def test_convert_to_camelcase():
 @pytest.mark.parametrize("input, output", errors)
 def test_export_error_matcher(input, output):
     
-    result = export_error_matcher(input, 'expense_report')
+    result = export_error_matcher(input, 'er')
     assert result == output
