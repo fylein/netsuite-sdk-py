43eeb1e501f288f6c893865daf589e90a7f7c010
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2023-06-16 11:14:58 +0530

    use keys we store in destination attribute db and remove loggers (#120)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='netsuitesdk',
-    version='2.19.1',
+    version='2.19.2',
     author='Siva Narayanan',
     author_email='siva@fyle.in',
     description='Python SDK for accessing the NetSuite SOAP webservice',
