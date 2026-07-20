abc05e183bdb01c7e52f1edceed4629ad65bd029
ashwin1111 <winash1998@gmail.com>
2026-07-20 22:45:02 +0530

    feat: introduce count() for custom record

--- netsuitesdk/api/custom_record_types.py
+++ netsuitesdk/api/custom_record_types.py
@@ -11,11 +11,19 @@ class CustomRecordTypes(ApiBase):
     def __init__(self, ns_client):
         ApiBase.__init__(self, ns_client=ns_client, type_name='CustomRecordType')
 
-    def get_all_by_id(self, internalId):
-        cr_type = self.ns_client.CustomRecordSearchBasic(
+    def _search_record(self, internalId):
+        return self.ns_client.CustomRecordSearchBasic(
             recType=self.ns_client.CustomRecordType(
                 internalId=internalId
             )
         )
+
+    def count(self, internalId):
+        cr_type = self._search_record(internalId)
+        ps = PaginatedSearch(client=self.ns_client, type_name='CustomRecordType', search_record=cr_type, pageSize=1)
+        return ps.total_records
+
+    def get_all_by_id(self, internalId):
+        cr_type = self._search_record(internalId)
         ps = PaginatedSearch(client=self.ns_client, type_name='CustomRecordType', search_record=cr_type, pageSize=20)
         return list(self._paginated_search_to_generator(paginated_search=ps))
